f08e3ea5a379072bc11ccd660b5742ed8e84e8bf
hbahadorzadeh <h.bahadorzadeh@gmail.com>
2013-08-22 22:54:51 +0430

    all headers in one file

---
+++ php_headers/headers.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'vars.php';
+include_once 'db_functions.php';
+include_once 'jalaly.php';
+include_once 'functions.php';
+include_once 'voting/voting.php';
+include_once 'user.php';
+
+my_session_start();
+?>
