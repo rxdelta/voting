910a8d93238012c37ef6baf9142db66aed86f6bc
Mostafa <me@mnazari.ir>
2013-08-23 10:02:45 +0430

    first version of UI is ready before designing candidate show panel

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 	include_once "php_headers/uif.php";
+	include_once "php_headers/voting/uif.php";
 	getHeader('صفحه اصلی');
-?>
- Test
-<?php
+	
+	$user = array(
+		array('u_name','نام','مصطفی نظری','false'),
+		array('u_username','نام کاربری','rxdelta','true'),
+		array('u_username','شناسه عضویت','3840103','false'),
+		array('u_graddate','سال دانش‌آموختگی','1384','false'),
+		array('u_grant','اعتبار عضویت','یک سال','false')		
+	);
+	
+	$app = array(
+		array('1','انتخابات هیئت مدیره انجمن'),
+		array('2','انتخابات بازرس هیئت مدیره انجمن'),
+	);
+	showUserInfo($user,$app);
+	
 	getFooter();
 ?>
